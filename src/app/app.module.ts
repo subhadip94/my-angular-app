a8d19604c1f7a66051c2745382aaae0548eb0b9c
Subhadip Jana <subhadip_j@trisysit.com>
2023-03-31 17:15:55 +0530

    basic route added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 
+const appRoutes: Routes = [
+  {path:'', component: AppComponent},
+  {path:'server', component: ServerComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
+value: any = '';
+name: string | null = '';
+show: boolean = false;
+error: boolean = false;
 
+onEnter(value: string) {
+  this.value = value;
+  if (this.value.length > 3) {
+    this.show = true;
+    this.error = false;
+  } else {
+    this.show = false;
+    this.error = true;
+  }
+}
+onChange(e: Event) {
+  this.name = (<HTMLInputElement>e.target).value;
+  console.log((<HTMLInputElement>e.target).value)
+}
 }
